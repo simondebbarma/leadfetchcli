3ad1b7bf41dc0785e58cd16d983b9fb85b6b3dae
Simon Debbarma <simonpeterdebbarma@outlook.com>
2023-11-24 02:47:17 +0530

    add jest for tests

--- __tests__/index.jest.js
+++ __tests__/index.jest.js
@@ -9,40 +9,4 @@ const { searchPlaces } = require('../index'); // assuming the function is export
 jest.mock('@google/maps');
 jest.mock('exceljs');
 
-describe('searchPlaces', () => {
-  it('should call googleMapsClient.places with correct parameters and return results', async () => {
-    const mockPlaces = jest.fn().mockResolvedValue({
-      json: {
-        results: ['place1', 'place2']
-      }
-    });
-    googleMapsClient.places = mockPlaces;
-
-    const results = await searchPlaces('keyword', 'location');
-
-    expect(mockPlaces).toHaveBeenCalledWith({
-      query: 'keyword in location',
-      language: 'en',
-    });
-    expect(results).toEqual(['place1', 'place2']);
-  });
-});
-
-describe('commander program action', () => {
-  it('should call searchPlaces and ExcelJS methods with correct parameters', async () => {
-    const mockSearchPlaces = jest.fn().mockResolvedValue(['place1', 'place2']);
-    const mockWorkbook = { addWorksheet: jest.fn(), xlsx: { writeFile: jest.fn() } };
-    const mockAddWorksheet = jest.fn().mockReturnValue({ columns: [], addRows: jest.fn() });
-    mockWorkbook.addWorksheet = mockAddWorksheet;
-    ExcelJS.Workbook = jest.fn().mockReturnValue(mockWorkbook);
-
-    // assuming the action function is exported and can be called directly
-    await action({ keyword: 'keyword', location: 'location' });
-
-    expect(mockSearchPlaces).toHaveBeenCalledWith('keyword', 'location');
-    expect(ExcelJS.Workbook).toHaveBeenCalled();
-    expect(mockAddWorksheet).toHaveBeenCalledWith('Google Maps Data');
-    expect(mockWorkbook.addWorksheet().addRows).toHaveBeenCalledWith(['place1', 'place2']);
-    expect(mockWorkbook.xlsx.writeFile).toHaveBeenCalledWith('GoogleMapsData.xlsx');
-  });
-});
+// TODO: write tests
